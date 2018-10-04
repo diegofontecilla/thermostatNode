205bf214aecd78e7a38df74ca34174b0170eabcb
diego fontecilla <mail@diegofontecilla.com>
2018-10-04 11:52:24 +0100

    Small refactor on play method

--- fizzBuzz/src/FizzBuzz.js
+++ fizzBuzz/src/FizzBuzz.js
@@ -2,17 +2,17 @@ function FizzBuzz() {
 }
 
 FizzBuzz.prototype.play = function(number) {
-  if (this._isDivisibleBy(15, number)) {
+  if (this._isDivisibleBy(number, 15)) {
     return 'FizzBuzz';
-  } else if (this._isDivisibleBy(5, number)) {
+  } else if (this._isDivisibleBy(number, 5)) {
     return 'Buzz';
-  } else if (this._isDivisibleBy(3, number)) {
+  } else if (this._isDivisibleBy(number, 3)) {
     return 'Fizz';
   } else {
     return number;
   }
 }
 
-FizzBuzz.prototype._isDivisibleBy = function(divisor, number) {
+FizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
   return number % divisor === 0;
 }
