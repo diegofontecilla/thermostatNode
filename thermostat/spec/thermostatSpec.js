bb176fe04744057c6c2d800330526d1b3967b206
diego fontecilla <mail@diegofontecilla.com>
2018-10-10 15:58:10 +0100

    Thermostat has a default temperature

---
+++ thermostat/spec/thermostatSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Thermostat', function(){
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+
+  it('Starts at 20 degrees by default', function(){
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+});
