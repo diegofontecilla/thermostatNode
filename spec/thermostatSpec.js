88cc8a4235bdd35660ceb803597c8972e4ac7247
diego fontecilla <mail@diegofontecilla.com>
2018-10-11 10:09:23 +0100

    thermostat has maximum and minimum temperatures

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -20,4 +20,26 @@ describe('Thermostat', function(){
     thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
+
+  it('maximum temperature limit is 32 degrees', function(){
+    for (var i = 1; i < 19; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(32);
+  });
+
+  it('minimum temperature limit is 10 degrees', function(){
+    for (var i = 1; i < 12; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it('checks if temperature is set to minumum temperature', function(){
+    expect(thermostat.isMinimumTemperature()).toBe(false);
+  });
+
+  it('checks if temperature is set to maximum temperature', function(){
+    expect(thermostat.isMaximumTemperature()).toBe(false)
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,9 @@
 'use strict';
 
 function Thermostat(){
+  this.MINIMUM_TEMPERATURE = 10;
+  this.MAXIMUM_TEMPERATURE = 32;
+  this.SAVE_MODE_TEMPERATURE = 25;
   this._temperature = 20;
 };
 
@@ -9,9 +12,23 @@ Thermostat.prototype.getCurrentTemperature = function(){
 };
 
 Thermostat.prototype.up = function(){
+  if (this.isMaximumTemperature()) {
+    return;
+  }
   return this._temperature += 1;
 };
 
-Thermostat.prototype.down = function () {
+Thermostat.prototype.down = function(){
+  if (this.isMinimumTemperature()) {
+    return;
+  }
   return this._temperature -= 1;
+};
+
+Thermostat.prototype.isMinimumTemperature = function(){
+  return this._temperature === this.MINIMUM_TEMPERATURE;
+};
+
+Thermostat.prototype.isMaximumTemperature = function(){
+  return this._temperature === this.MAXIMUM_TEMPERATURE;
 };
