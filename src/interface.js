a97f13be401283197e8f59f111f81b2d2f0fb663
diego fontecilla <mail@diegofontecilla.com>
2018-10-12 15:43:54 +0100

    Connecting to weather API

--- src/interface.js
+++ src/interface.js
@@ -29,6 +29,28 @@ $(document).ready(function(){
     updateTemperature();
   });
 
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=d46acf81288f25a5981e588e1be618ee';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
+  };
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=d46acf81288f25a5981e588e1be618ee&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
+  })
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=d46acf81288f25a5981e588e1be618ee&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+  })
+
   function updateTemperature(){
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
